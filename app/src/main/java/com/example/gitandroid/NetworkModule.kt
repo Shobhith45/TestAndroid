051adf60f6f4b4d6c852e41c682fbf5f958e0646
shobith <shobith@robosoftin.com>
2022-05-18 20:12:51 +0530

    added new logic module

--- app/src/main/java/com/example/gitandroid/NetworkModule.kt
+++ app/src/main/java/com/example/gitandroid/NetworkModule.kt
@@ -3,11 +3,7 @@ package com.example.gitandroid
 object NetworkModule {
 
     fun provideRetrofit(): String {
-        val name = "shobhith"
-        val age = 20
-        val mobile = "sss"
-        20 + 30
-        //random logic
-        return name
+        //I will change logic here
+        return "hello world"
     }
 }
